bde14c60bf07442a470242f0806911a89ca82ea1
Matt <MattP598@gmail.com>
2022-04-27 04:43:23 -0500

    Selection Statement Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -6,7 +6,29 @@ namespace SelectionStatementExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var r = new Random();
+            int favoriteNum = r.Next(1, 1000);
+            int guess;
+
+            Console.WriteLine("Welcome to the number guessing game!");
+            
+            Console.WriteLine("Please enter a number between 1 and 1000: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > favoriteNum)
+            {
+                Console.WriteLine("Your guess is too high!");
+                Console.WriteLine("The correct number was "+favoriteNum);
+            }
+            else if (guess < favoriteNum)
+            {
+                Console.WriteLine("Your guess is too low!");
+                Console.WriteLine("The correct number was " + favoriteNum);
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
         }
     }
 }
